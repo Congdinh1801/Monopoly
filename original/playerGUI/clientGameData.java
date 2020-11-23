db99d80d77f667b4e4c2ff1297af48807805078d
Congdinh1801 <Congdinh1801@yahoo.com>
2020-11-23 14:18:34 -0600

    clientgamedata - dinh H

--- original/playerGUI/clientGameData.java
+++ original/playerGUI/clientGameData.java
@@ -1,6 +1,23 @@
-//package playerGUI;
-//
-//public class clientGameData {
-//	private Player;
-//	private Dice
-//}
+package playerGUI;
+
+import java.io.Serializable;
+
+import serverBackend.dice.Dice;
+
+public class clientGameData implements Serializable  {
+	private Dice dice1;
+	private Dice dice2;
+	public Dice getDice1() {
+		return dice1;
+	}
+	public void setDice1(Dice dice1) {
+		this.dice1 = dice1;
+	}
+	public Dice getDice2() {
+		return dice2;
+	}
+	public void setDice2(Dice dice2) {
+		this.dice2 = dice2;
+	}
+
+}
